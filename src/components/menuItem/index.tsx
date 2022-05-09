c55bc7948f30933f183234847e00fc43783afb58
Damien Sedgwick <damiensedgwick@icloud.com>
2022-05-09 21:37:20 +0100

    Added prisma config and migration

--- src/components/menuItem/index.tsx
+++ src/components/menuItem/index.tsx
@@ -1,12 +1,12 @@
-import NextLink from "next/link";
-import { LinkBox, LinkOverlay, ListIcon } from "@chakra-ui/layout";
+import NextLink from 'next/link';
+import { LinkBox, LinkOverlay, ListIcon } from '@chakra-ui/layout';
 
 export const MenuItem = ({ item }) => {
   return (
     <LinkBox>
       <NextLink href={item.route} passHref>
         <LinkOverlay>
-          <ListIcon as={item.icon} marginRight="20px" color="white" />
+          <ListIcon as={item.icon} marginRight='20px' color='white' />
           {item.name}
         </LinkOverlay>
       </NextLink>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
-import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
-import "reset-css";
-import { theme } from "styles/theme";
-import { Layout } from "components/layout";
+import type { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
+import 'reset-css';
+import { theme } from 'styles/theme';
+import { Layout } from 'components/layout';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
